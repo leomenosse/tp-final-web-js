f1b3b714224357f3b074721c394969a2900930ca
Leonardo Menosse <leo_menosse@hotmail.com>
2021-02-02 18:45:02 -0300

    leitura de produtos a partir de json

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 let menuBar = document.querySelector(".left-menubar")
+let content = document.querySelector(".content")
+
+function getCategoriasUnicas(produtos){
+    let categorias = []
+    
+    for (const produto of produtos) {
+        categorias.push(produto["categoria"])
+    }
+    return [...new Set(categorias)]
+}
+let categoriasUnicas = getCategoriasUnicas(produtos)
 
 //preenchendo os itens dinamicamente no menu da esquerda
-for(let i = 0; i < 10; i++){
+for(let i = 0; i < categoriasUnicas.length; i++){
     let item = document.createElement("div")
     item.style.width = "100%"
     item.style.height = "10%"
@@ -10,27 +21,27 @@ for(let i = 0; i < 10; i++){
     item.style.display = "flex"
     item.style.justifyContent = "center"
     item.style.alignItems = "center"
-    item.innerHTML = `Item ${i + 1}`
+    item.innerHTML = categoriasUnicas[i]
 
     menuBar.appendChild(item)
 }
 
-let content = document.querySelector(".content")
-let colors = ["yellow", "green", "olive", "crimson", "white", "purple", "grey", "aquamarine", "seagreen", "palevioletred", "brown", "orange"]
 
 //preenchendo dinamicamente o conteudo principal
-for(let i = 0; i < 12; i++){
+for(let i = 0; i < produtos.length; i++){
     let card = document.createElement("div")
     card.className = "card"
-    card.style.backgroundColor = colors[i]
+    card.style.backgroundColor = "rgba(255,255,255,0.8)"
     card.style.borderRadius = "5px"
     card.style.padding = "20px 0"
     card.style.fontWeight = "bold"
+    card.style.minHeight = "300px"
     
     card.style.display = "flex"
     card.style.justifyContent = "center"
     card.style.alignItems = "center"
-    card.innerHTML = `Item ${i + 1}`
+    
+    card.innerHTML = produtos[i]["nome"]
 
     content.appendChild(card)
 }
---
+++ js/produtos.js
@@ -0,0 +1,32 @@
+let produtos = [
+    {
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 995.90,
+        "imgPath": "../img/geladeira.jpg"
+    },
+    {
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 4000.00,
+        "imgPath": "../img/notebook.jpg"
+    },
+    {
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 500.00,
+        "imgPath": "../img/guarda-roupa.jpg"
+    },
+    {
+        "nome": "Camiseta",
+        "categoria": "Vestuário",
+        "preco": 60.00,
+        "imgPath": "../img/camiseta.jpg"
+    },
+    {
+        "nome": "Fone de ouvido",
+        "categoria": "Eletrônicos",
+        "preco": 200.00,
+        "imgPath": "../img/fone-de-ouvido.jpg"
+    }
+]
