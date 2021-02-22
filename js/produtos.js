846408f6937c191a9e5083d94991f0dea7e5753d
thalesvini <thales.britoue@gmail.com>
2021-02-21 22:10:50 -0300

    adição de mais produtos à página principal

--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,4 @@
-let produtos = [
-    {
+let produtos = [{
         "id": 0,
         "nome": "Geladeira",
         "categoria": "Eletrodomésticos",
@@ -54,5 +53,173 @@ let produtos = [
         "categoria": "Móveis",
         "preco": 500.00,
         "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 8,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 550.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 9,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 1499.90,
+        "imgPath": "../img/geladeira.png"
+    },
+    {
+        "id": 10,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 650.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 11,
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 4599.99,
+        "imgPath": "../img/notebook.png"
+    },
+    {
+        "id": 12,
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 7000.00,
+        "imgPath": "../img/notebook.png"
+    },
+    {
+        "id": 13,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 1200.00,
+        "imgPath": "../img/geladeira.png"
+    },
+    {
+        "id": 14,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 349.99,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 15,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 900.00,
+        "imgPath": "../img/geladeira.png"
+    },
+    {
+        "id": 16,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 799.90,
+        "imgPath": "../img/geladeira.png"
+    },
+    {
+        "id": 17,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 400.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 18,
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 7300.00,
+        "imgPath": "../img/notebook.png"
+    },
+    {
+        "id": 19,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 7149.90,
+        "imgPath": "../img/geladeira.png"
+    },
+    {
+        "id": 20,
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 7499.99,
+        "imgPath": "../img/notebook.png"
+    },
+    {
+        "id": 21,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 499.90,
+        "imgPath": "../img/geladeira.png"
+    },
+    {
+        "id": 22,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 1400.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 23,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 4199.90,
+        "imgPath": "../img/geladeira.png"
+    },
+    {
+        "id": 24,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 4000.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 25,
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 6499.99,
+        "imgPath": "../img/notebook.png"
+    },
+    {
+        "id": 26,
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 6200.00,
+        "imgPath": "../img/notebook.png"
+    },
+    {
+        "id": 27,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 6000.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 28,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 4500.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 29,
+        "nome": "Guarda roupa",
+        "categoria": "Móveis",
+        "preco": 4500.00,
+        "imgPath": "../img/guarda-roupa.png"
+    },
+    {
+        "id": 30,
+        "nome": "Notebook",
+        "categoria": "Eletrônicos",
+        "preco": 4999.99,
+        "imgPath": "../img/notebook.png"
+    },
+    {
+        "id": 31,
+        "nome": "Geladeira",
+        "categoria": "Eletrodomésticos",
+        "preco": 4700.00,
+        "imgPath": "../img/geladeira.png"
     }
 ]
