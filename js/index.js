f708cb42953ed44694a66904e2fabf700885f748
thalesvini <thales.britoue@gmail.com>
2021-02-22 22:14:57 -0300

    ajuste das img dos produtos

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ let filtroPrecos = document.querySelector("#precos")
 let content = document.querySelector(".card-container")
 
 //retorna a quantidade de um item no carrinho de compras
-function getQtdNoCarrinho(id){
+function getQtdNoCarrinho(id) {
     let carrinho = JSON.parse(sessionStorage.getItem('carrinho'))
 
-    if(carrinho != null && carrinho.length > 0){
-        for(let i = 0; i < carrinho.length; i++){
-            if(carrinho[i].id == id){
-                console.log("Quantidade do item " + id+ " no carrinho: " + carrinho[i].qtd)
+    if (carrinho != null && carrinho.length > 0) {
+        for (let i = 0; i < carrinho.length; i++) {
+            if (carrinho[i].id == id) {
+                console.log("Quantidade do item " + id + " no carrinho: " + carrinho[i].qtd)
                 return carrinho[i].qtd
             }
         }
@@ -20,20 +20,20 @@ function getQtdNoCarrinho(id){
 
 //adiciona o item no carrinho (sessionStorage)
 //atualiza o texto do botão do item, indicando a quantidade deles no carrinho
-function addNoCarrinho(){
+function addNoCarrinho() {
     let estaNoCarrinho = false
     let carrinho = JSON.parse(sessionStorage.getItem('carrinho'))
 
-    if(carrinho != null && carrinho.length > 0){
-        for(let i = 0; i < carrinho.length; i++){
-            if(carrinho[i].id == this.name){
+    if (carrinho != null && carrinho.length > 0) {
+        for (let i = 0; i < carrinho.length; i++) {
+            if (carrinho[i].id == this.name) {
                 estaNoCarrinho = true
                 carrinho[i].qtd++
-                this.innerHTML = "Quantidade no carrinho: " + carrinho[i].qtd
+                    this.innerHTML = "Quantidade no carrinho: " + carrinho[i].qtd
             }
         }
-        
-        if(!estaNoCarrinho){ //se o item não estiver no carrinho, ele é adicionado
+
+        if (!estaNoCarrinho) { //se o item não estiver no carrinho, ele é adicionado
             this.innerHTML = "Quantidade no carrinho: 1"
             let item = {
                 "id": this.name,
@@ -43,8 +43,7 @@ function addNoCarrinho(){
         }
 
         sessionStorage.setItem('carrinho', JSON.stringify(carrinho))
-    }
-    else{
+    } else {
         let item = {
             "id": this.name,
             "qtd": 1
@@ -69,54 +68,53 @@ let categorias = getCategorias(produtos)
 //exibição de produtos na tela inicial
 //produtos é a lista com os produtos
 //content é o container onde os produtos serão exibidos
-function exibirProdutos(produtos, content){
+function exibirProdutos(produtos, content) {
     for (let i = 0; i < produtos.length; i++) {
         let card = document.createElement("div")
         card.className = "card"
-    
+
         let img = document.createElement("img")
         img.className = "fotoProduto"
         img.src = produtos[i]["imgPath"]
-    
+
         let nome = document.createElement("div")
         nome.className = "nome"
-        nome.innerHTML = produtos[i]["nome"]
-    
+        nome.innerHTML = "</br>" + produtos[i]["nome"]
+
         let preco = document.createElement("p")
         preco.className = "preco"
         preco.innerHTML = `R$${produtos[i]["preco"].toFixed(2)}`
-    
+
         let comprar = document.createElement("button")
         comprar.className = "botaoComprar"
         let qtdNoCarrinho = getQtdNoCarrinho(produtos[i]["id"])
-        if(qtdNoCarrinho == 0){
+        if (qtdNoCarrinho == 0) {
             comprar.innerHTML = "Adicionar ao carrinho"
-        }
-        else{
+        } else {
             comprar.innerHTML = `Quantidade no carrinho: ${qtdNoCarrinho}`
-        }        
+        }
         comprar.name = produtos[i]["id"]
         comprar.addEventListener("click", addNoCarrinho)
-    
+
         card.appendChild(img)
         card.appendChild(nome)
         card.appendChild(preco)
         card.appendChild(comprar)
-    
+
         content.appendChild(card)
     }
 }
 exibirProdutos(produtos, content)
 
 //preenchendo os itens dinamicamente no menu da esquerda
-for(let i = 0; i < categorias.length; i++){
+for (let i = 0; i < categorias.length; i++) {
     let categoria = document.createElement("input")
-    categoria.type = "checkbox" 
+    categoria.type = "checkbox"
     categoria.value = categorias[i]
     categoria.id = categorias[i]
     categoria.name = categorias[i]
     categoria.className = "checkbox-categoria"
-    
+
     let label = document.createElement("label")
     label.setAttribute("for", categorias[i])
     label.innerHTML = categorias[i]
@@ -128,18 +126,18 @@ for(let i = 0; i < categorias.length; i++){
     filtroCategorias.appendChild(lineBreak)
 }
 
-function setFiltroPreco(){
+function setFiltroPreco() {
     let inicioIntervalos = []
 
-    for(let i = 0; i < produtos.length; i++){
-        let preco = produtos[i]["preco"]      
+    for (let i = 0; i < produtos.length; i++) {
+        let preco = produtos[i]["preco"]
         inicioIntervalos.push(parseInt(preco / 500) * 500) //adicionando o início do intervalo no set
     }
 
     //coloca em um Set para eliminar os repetidos e aplica uma ordenação
     inicioIntervalos = [...(new Set(inicioIntervalos))].sort((a, b) => a - b)
 
-    inicioIntervalos.forEach((inicio)=>{
+    inicioIntervalos.forEach((inicio) => {
         let preco = document.createElement("input")
         preco.type = "checkbox"
         preco.value = inicio
